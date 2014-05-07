128756068a1c6d77ab7fe00dbe11dfef41a0acca
svilerino <svilerino@gmail.com>
2014-05-07 18:53:35 -0300

    terminado codigo ej3

--- tp2/Ej3-SaltosEnLaMatrix/src/Main.java
+++ tp2/Ej3-SaltosEnLaMatrix/src/Main.java
@@ -1,9 +1,38 @@
 import java.util.List;
+import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class Main {
 
+	private static int powerUpInicial;	
+	private static int filaInicial;
+	private static int columnaInicial;
+	private static int filaDestino;
+	private static int columnaDestino;
+	private static int[][] matrix;
+	private static int matrixDim;
+	
 	public static void parseInput(){
-		//...TODO: complete this
+		Scanner in = new Scanner(System.in);
+		String firstLine = in.nextLine();
+		//dimension fo co fd cd powerupDisp
+		String[] splitUp = firstLine.split(" ");
+		matrixDim = Integer.parseInt(splitUp[0]);
+		filaInicial = Integer.parseInt(splitUp[1]);
+		columnaInicial = Integer.parseInt(splitUp[2]);
+		filaDestino = Integer.parseInt(splitUp[3]);
+		columnaDestino = Integer.parseInt(splitUp[4]);
+		powerUpInicial = Integer.parseInt(splitUp[5]);
+		
+		matrix = new int[matrixDim][matrixDim];
+		
+		for(int i=0;i<matrixDim;i++){
+			String line = in.nextLine();
+			StringTokenizer st = new StringTokenizer(line);
+			for(int j=0;j<matrixDim;j++){
+				matrix[i][j] = Integer.parseInt(st.nextToken(" "));				
+			}							
+		}
 	}
 	
 	public static String serializeOutput(List<Nodo> caminoOptimo){
@@ -22,15 +51,10 @@ public class Main {
 	}
 	
 	public static void main(String[] args) {
-		int[][] matrix = {{1, 1, 1}, {1, 1, 1}, {1, 1, 1}};
-		//int[][] matrix = {{1, 1}, {1, 1}};
-		int powerUpInicial = 2;
+		parseInput();
+		//matrix = {{1, 1, 1}, {1, 1, 1}, {1, 1, 1}};
+		//matrix = {{1, 1}, {1, 1}};
 		
-		int filaInicial = 0;
-		int columnaInicial = 0;
-		int filaDestino = 2 ;
-		int columnaDestino = 2;
-		//Nodo inicial y destino no distinguen el nivel		
 		Juego juego = new Juego(matrix, powerUpInicial, filaInicial, columnaInicial, filaDestino, columnaDestino);
 		//System.out.println(juego.getGraphString());
 		System.out.println(serializeOutput(juego.caminoMinimo()));
