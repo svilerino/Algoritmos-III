e23b5c0506497f759c2c928fadb23c2d6cc254e6
Nico <ndato@3way.com.ar>
2014-05-07 23:07:40 -0300

    agrego implementacion de grafo

--- tp2/ej2/ej2.cpp
+++ tp2/ej2/ej2.cpp
@@ -9,6 +9,104 @@ using namespace std;
 
 int calcular_tiempo = 0;
 
+Grafo *crear_grafo(int nodos)
+{
+	int i;
+
+	Grafo *g = NULL;
+	if(nodos <= 0)
+		return NULL;
+
+	g = (Grafo *)malloc(sizeof(Grafo));
+	g->nodos = nodos;
+	g->componente_conexa_nodos = (int *)calloc(nodos, sizeof(int));
+	g->componentes_conexas = nodos;
+	g->adyacencia = (int **)malloc(sizeof(int *) * nodos);
+
+	for(i = 0; i < nodos; i++) {
+		g->adyacencia[i] = (int *)calloc(nodos, sizeof(int));
+	}
+	for(i = 0; i < nodos; i++) {
+		g->componente_conexa_nodos[i] = i;
+	}
+	return g;
+}
+
+void liberar_grafo(Grafo *g)
+{
+	int i;
+
+	if(!g)
+		return;
+
+	free(g->componente_conexa_nodos);
+	for(i = 0; i < g->nodos; i++) {
+		free(g->adyacencia[i]);
+	}
+	free(g->adyacencia);
+	free(g);
+}
+
+int agregar_arista(Grafo *g, Nodo nodo1, Nodo nodo2)
+{
+	int i;
+	int nueva_componente, vieja_componente;
+
+	if(!g)
+		return -1;
+
+	if(nodo1 >= g->nodos || nodo2 >= g->nodos)
+		return -1;
+
+	g->adyacencia[nodo1][nodo2] = 1;
+	g->adyacencia[nodo2][nodo1] = 1;
+
+	if(g->componente_conexa_nodos[nodo1] != g->componente_conexa_nodos[nodo2]){
+		if(g->componente_conexa_nodos[nodo1] <= g->componente_conexa_nodos[nodo2]){
+			nueva_componente = g->componente_conexa_nodos[nodo1];
+			vieja_componente = g->componente_conexa_nodos[nodo2];
+		}
+		else{
+			nueva_componente = g->componente_conexa_nodos[nodo2];
+			vieja_componente = g->componente_conexa_nodos[nodo1];
+		}
+
+		for(i = 0; i < g->nodos; i++){ //actualizo la componente conexa de todos los nodos que pertenecian a esa componente
+			if(g->componente_conexa_nodos[i] == vieja_componente){
+				g->componente_conexa_nodos[i] = nueva_componente;
+			}
+		}
+		g->componentes_conexas--;
+	}
+	return 0;
+}
+
+int cantidad_componentes_conexas(Grafo *g)
+{
+	if(!g)
+		return -1;
+	return g->componentes_conexas;
+}
+
+//retorna un vector con cantidad_componentes_conexas() elementos, en cada iesimo elemento, hay un nodo correspondiente a la componente iesima. liberar el resultado con free()
+Nodo *nodos_de_componentes(Grafo *g)
+{
+	Nodo *nodos;
+	int i;
+
+	if(!g)
+		return NULL;
+
+	nodos = (Nodo *)malloc(sizeof(Nodo) * g->nodos);
+	for(i = 0; i < g->nodos; i++){
+		nodos[i] = -1;
+	}
+	for(i = 0; i < g->componentes_conexas; i++){
+		nodos[g->componente_conexa_nodos[i]] = i;
+	}
+	return nodos;
+}
+
 int resolver(void)
 {
 	return 0;
--- tp2/ej2/ej2.h
+++ tp2/ej2/ej2.h
@@ -3,6 +3,38 @@
 
 #define REPETICIONES_CALCULAR_TIEMPO (200)
 
+#ifndef NULL
+#define NULL ((void *)0)
+#endif
+
+#ifndef MAX
+#define MAX(a,b) ((a)>(b)?(a):(b))
+#endif
+
+#ifndef MIN
+#define MIN(a,b) ((a)<(b)?(a):(b))
+#endif
+
+typedef int Nodo;
+
+struct GrafoMatrizAdyacencia{
+	int nodos; //cantidad de nodos
+	int *componente_conexa_nodos; //array para asignar componentes conexas
+	int componentes_conexas;
+	int **adyacencia; //la matriz ed adyacencia
+};
+
+typedef struct GrafoMatrizAdyacencia Grafo;
+
+//nodos = cantidad de nodos del grafo. crea el grafo con nodos que van de 0 a nodos - 1
+Grafo *crear_grafo(int nodos);
+void liberar_grafo(Grafo *g);
+//nodo1 y nodo2 tienen que ir de 0 a nodos - 1
+int agregar_arista(Grafo *g, Nodo nodo1, Nodo nodo2);
+int cantidad_componentes_conexas(Grafo *g);
+//retorna un vector con tantos nodos en el grafo como elementos elementos, en cada iesimo elemento, hay un nodo correspondiente a la componente iesima. si esa componente conexa ya no existe, se pone el valor -1. liberar el resultado con free()
+Nodo *nodos_de_componentes(Grafo *g);
+
 int resolver(void);
 
 #endif //__EJ2_H__
