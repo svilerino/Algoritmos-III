45d3741783e8a5ed47162f24f92b919ba564d817
Nico D <ndato@3way.com.ar>
2014-06-08 01:47:45 -0300

    agrego implementacion de grafo en lista y arreglo perdida de memoria

--- tp3/codigo/common/grafo.h
+++ tp3/codigo/common/grafo.h
@@ -65,7 +65,7 @@ template <class W> class GrafoAdyacencia : Grafo<W> {
 			for(i = 0; i < nodos; i++){
 				free(adyacencia[i]);
 			}
-			free(adyacencia[i]);
+			free(adyacencia);
 		}
 
 		/**
@@ -136,6 +136,161 @@ template <class W> class GrafoAdyacencia : Grafo<W> {
 					ret[*m] = j + 1;
 					(*m)++;
 				}
+			}
+			return ret;
+		}
+};
+
+template <class W> class GrafoLista : Grafo<W> {
+	private:
+		typedef struct Lista {
+			struct Lista *siguiente;
+			W *peso;
+			int nodo;
+		} Lista;
+		Lista **lista_nodos;
+		int nodos;
+
+	public:
+		/**
+		 * O(1)
+		 */
+		GrafoLista(int n){
+			nodos = n;
+			lista_nodos = (Lista **)calloc(n, sizeof(Lista *));
+		}
+
+		/**
+		 * O(nodos + aristas)
+		 */
+		~GrafoLista(){
+			Lista *l, *l2;
+			int i;
+			for(i = 0; i < nodos; i++){
+				l = lista_nodos[i];
+				while(l != NULL){
+					l2 = l;
+					l = l->siguiente;
+					free(l2);
+				}
+			}
+			free(lista_nodos);
+		}
+		/**
+		 * Agrega una arista entre i y j, 1 <= i <= n, 1 <= j <= n, n cantidad de nodos.
+		 * O(1)
+		 */
+		bool AgregarArista(int i, int j, W *peso){
+			Lista *nuevo;
+
+			if(i <= 0 || i > nodos || j <= 0 || j > nodos)
+				return false;
+
+			nuevo = (Lista *)malloc(sizeof(Lista));
+			nuevo->nodo = j;
+			nuevo->peso = peso;
+			nuevo->siguiente = lista_nodos[i - 1];
+			lista_nodos[i - 1] = nuevo;
+
+			nuevo = (Lista *)malloc(sizeof(Lista));
+			nuevo->nodo = i;
+			nuevo->peso = peso;
+			nuevo->siguiente = lista_nodos[j - 1];
+			lista_nodos[j - 1] = nuevo;
+
+			return true;
+		}
+		/**
+		 * Quita la arista entre i y j, 1 <= i <= n, 1 <= j <= n, n cantidad de nodos.
+		 * O(aristas)
+		 */
+		bool QuitarArista(int i, int j){
+			Lista *l, *p;
+			if(i <= 0 || i > nodos || j <= 0 || j > nodos)
+				return false;
+
+			l = lista_nodos[i - 1];
+			p = NULL;
+			while(l != NULL){
+				if(l->nodo == j){
+					if(p != NULL)
+						p->siguiente = l->siguiente;
+					else
+						lista_nodos[i - 1] = l->siguiente;
+					free(l);
+				}
+				p = l;
+				l = l->siguiente;
+			}
+
+			l = lista_nodos[j - 1];
+			p = NULL;
+			while(l != NULL){
+				if(l->nodo == i){
+					if(p != NULL)
+						p->siguiente = l->siguiente;
+					else
+						lista_nodos[i - 1] = l->siguiente;
+					free(l);
+				}
+				p = l;
+				l = l->siguiente;
+			}
+			return true;
+		}
+
+		/**
+		 * O(aristas)
+		 */
+		bool EsAdyacente(int i, int j){
+			Lista *l;
+			if(i <= 0 || i > nodos || j <= 0 || j > nodos)
+				return false;
+
+			l = lista_nodos[i - 1];
+			while(l != NULL){
+				if(l->nodo == j){
+					return true;
+				}
+				l = l->siguiente;
+			}
+			return false;
+		}
+		/**
+		 * i y j tienen que ser adyacentes, sino retorna NULL.
+		 */
+		W *Peso(int i, int j){
+			Lista *l;
+			if(i <= 0 || i > nodos || j <= 0 || j > nodos)
+				return NULL;
+
+			l = lista_nodos[i - 1];
+			while(l != NULL){
+				if(l->nodo == j){
+					return l->peso;
+				}
+				l = l->siguiente;
+			}
+			return NULL;
+
+		}
+		/**
+		 * retorna un array de nodos adyacentes a i, en m retorna la cantidad de elementos en el array que se retorna. liberar con free()
+		 * O(aristas)
+		 */
+		int *Adyacentes(int i, int *m){
+			int *ret;
+			Lista *l;
+
+			if(!m || i <= 0 || i > nodos)
+				return NULL;
+			*m = 0;
+			ret = (int *)calloc(nodos, sizeof(int));
+			l = lista_nodos[i - 1];
+			while(l != NULL){
+				ret[*m] = l->nodo;
+				(*m)++;
+				l = l->siguiente;
 			}
 			return ret;
 		}
