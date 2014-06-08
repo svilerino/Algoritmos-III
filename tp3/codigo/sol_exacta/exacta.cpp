69296717ee21dbac2f0ab84d5518e6f1ae65bca7
Nico D <ndato@3way.com.ar>
2014-06-08 01:54:52 -0300

    agrego ejemplo de grafo en lista

--- tp3/codigo/sol_exacta/exacta.cpp
+++ tp3/codigo/sol_exacta/exacta.cpp
@@ -11,14 +11,30 @@ typedef struct peso{
 
 int main(int argc, char **argv)
 {
-	peso p1;
-	GrafoAdyacencia<peso> grafo(10);
+	{
+		peso p1, *p;
+		GrafoAdyacencia<peso> grafo(10);
 
-	p1.w1 = 42;
-	p1.w2 = 30;
-	grafo.AgregarArista(2,3, &p1);
+		p1.w1 = 42;
+		p1.w2 = 30;
+		grafo.AgregarArista(2,3, &p1);
 
-	cout << grafo.EsAdyacente(2,3) << " " << grafo.EsAdyacente(9,8);
+		cout << grafo.EsAdyacente(2,3) << " " << grafo.EsAdyacente(9,8) << endl;
+		p = grafo.Peso(2, 3);
+		cout << p->w1 << " " << p->w2 << " " << endl;
+	}
+	{
+		peso p1, *p;
+		GrafoLista<peso> grafo(10);
+
+		p1.w1 = 32;
+		p1.w2 = 20;
+		grafo.AgregarArista(5,3, &p1);
+
+		cout << grafo.EsAdyacente(5,3) << " " << grafo.EsAdyacente(2,3) << endl;
+		p = grafo.Peso(5, 3);
+		cout << p->w1 << " " << p->w2 << " " << endl;
+	}
 	return 0;
 }
 
