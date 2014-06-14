dd21edb4dcf136156a8c23535ff5eb88157fd65a
svilerino <svilerino@gmail.com>
2014-06-14 15:43:11 -0300

    comentarios iniciales de como se codea la bqlocal

--- tp3/codigo/bqlocal/bqlocal.cpp
+++ tp3/codigo/bqlocal/bqlocal.cpp
@@ -7,6 +7,13 @@
 using namespace std;
 
 int main(int argc, char **argv)
-{	
+{
+	//Sea G=(V,E) un grafo,
+	//Sean f,g  dos funciones de pesos en las aristas del grafo y sea K en R un limite para la funcion g
+	//Quiero un camino minimo sobre f pero tal que g<K entre dos nodos A y B
+	//Dado un grafo G, primero voy a buscar un camino inicial factible.
+	//Para esto voy a hacer dijkstra desde A, y me voy a quedar con el camino de A hasta B que minimice g
+	//si dicho camino se pasa de la cota K entonces no existe solucion.
+	//Caso contrario, este camino sera la solucion inicial factible para comenzar la busqueda local
 	return 0;
 }
