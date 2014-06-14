411c63df8c3b50e10bdb3c753591bc45e82dbe9d
svilerino <svilerino@gmail.com>
2014-06-14 15:09:01 -0300

    Makefiles fix

---
+++ tp3/codigo/bqlocal/bqlocal.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <iostream>
+#include <string.h>
+#include "../common/grafo.h"
+#include "../common/parser.h"
+
+using namespace std;
+
+int main(int argc, char **argv)
+{	
+	return 0;
+}
