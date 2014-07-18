ada5f0150af9b518dfe276d65f3b32e49b25f582
svilerino <svilerino@gmail.com>
2014-07-18 18:48:05 -0300

    parametros

--- tp3/codigo/heuristicas/create_tests.sh
+++ tp3/codigo/heuristicas/create_tests.sh
@@ -11,15 +11,15 @@ red='\e[0;31m'
 green='\e[0;32m'
 NC='\e[0m' # No Color
 
-cant_nodos_min=10
-cant_nodos_max=60
-#cant_nodos_min=100
-#cant_nodos_max=800
+#cant_nodos_min=10
+#cant_nodos_max=60
+cant_nodos_min=100
+cant_nodos_max=700
 peso_maximo_w1=250
 peso_maximo_w2=120
-limit_w1=1000
+limit_w1=500
 
-for (( n=$cant_nodos_min; n<=$cant_nodos_max; n+=5 ))
+for (( n=$cant_nodos_min; n<=$cant_nodos_max; n+=100 ))
 #for (( n=$cant_nodos_min; n<=$cant_nodos_max; n+=50 ))
 do 
 	#seleccionar aca abajo una combinacion de aristas... para establecer un valor fijo hay que poner max y min iguales.
@@ -35,7 +35,7 @@ do
 	echo "Generando grafos de $n nodos con cantidad de aristas aleatorias en el rango [$aristas_minimas .. $aristas_maximas]"
 	echo "Los pesos W1 de las aristas van a estar en un rango preestablecido aleatorio entre [1..$peso_maximo_w1]"
 	echo "Los pesos W2 de las aristas van a estar en un rango preestablecido aleatorio entre [1..$peso_maximo_w2]"
-	for (( a=$aristas_minimas; a<=$aristas_maximas; a+=500 ))
+	for (( a=$aristas_minimas; a<=$aristas_maximas; a+=1000 ))
 	#for (( a=$aristas_minimas; a<=$aristas_maximas; a+=4500 ))
 	do
 		echo -n "Generando grafo de $n nodos y $a aristas aleatorio..."
