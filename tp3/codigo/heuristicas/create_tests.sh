a510dff53c2d71f8427452e1edcd0357fc40ffce
svilerino <svilerino@gmail.com>
2014-07-23 19:26:53 -0300

    fisher gates random graph generator

--- tp3/codigo/heuristicas/create_tests.sh
+++ tp3/codigo/heuristicas/create_tests.sh
@@ -11,13 +11,13 @@ red='\e[0;31m'
 green='\e[0;32m'
 NC='\e[0m' # No Color
 
-cant_nodos_min=100
-cant_nodos_max=160
+cant_nodos_min=3
+cant_nodos_max=4
 peso_maximo_w1=250
 peso_maximo_w2=400
 limit_w1=200
 
-for (( n=$cant_nodos_min; n<=$cant_nodos_max; n+=20 ))
+for (( n=$cant_nodos_min; n<=$cant_nodos_max; n+=1 ))
 do 
 	#seleccionar aca abajo una combinacion de aristas... para establecer un valor fijo hay que poner max y min iguales.
 	#aristas_minimas=$((n * (n-1)/3))
@@ -29,8 +29,11 @@ do
 	#aristas_minimas=$((n * (n-1)/10))
 	#aristas_maximas=$((n * (n-1)/7))
 
-	aristas_minimas=$(echo "scale=0; $n*sqrt($n)" | bc -l )
-	aristas_maximas=$(echo "scale=0; 5*$n*sqrt($n)" | bc -l )
+	#aristas_minimas=$(echo "scale=0; $n*sqrt($n)" | bc -l )
+	#aristas_maximas=$(echo "scale=0; 5*$n*sqrt($n)" | bc -l )
+
+	aristas_minimas=$((n * (n-1)/2))
+	aristas_maximas=$((n * (n-1)/2))
 
 	echo "Generando grafos de $n nodos con cantidad de aristas aleatorias en el rango [$aristas_minimas .. $aristas_maximas]"
 	echo "Los pesos W1 de las aristas van a estar en un rango preestablecido aleatorio entre [1..$peso_maximo_w1]"
@@ -39,7 +42,8 @@ do
 	do
 		echo -n "Generando grafo de $n nodos y $a aristas aleatorio..."
 		#../generar_grafos/generar_grafos cantidad_nodos cantidad_aristas maximo_random_w1 maximo_random_w2
-		../generar_grafos/generar_grafos "$n" "$a" "$peso_maximo_w1" "$peso_maximo_w2" "$limit_w1" > "test-cases/instancia_$n.$a.in"
+		#../generar_grafos/generar_grafos "$n" "$a" "$peso_maximo_w1" "$peso_maximo_w2" "$limit_w1" > "test-cases/instancia_$n.$a.in"
+		./generador_fisher_gates "$n" "$a" "$peso_minimo_w1" "$peso_maximo_w1" "$peso_minimo_w2" "$peso_maximo_w2" "$limit_w1" > "test-cases/instancia_$n.$a.in"
 		echo "Ok!"
 	done
 	echo "-------------------------------------------------------------------------------------------------------------------------"
---
+++ tp3/codigo/heuristicas/generador_fisher_gates.cpp
@@ -0,0 +1,80 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <cstdlib>
+#include <random>
+
+using namespace std;
+
+int main(int argc, char** argv){
+	int cant_nodos = 0;
+	int cant_aristas = 0;
+	int pesomin_w1 = 0;
+	int pesomax_w1 = 0;
+	int pesomin_w2 = 0;
+	int pesomax_w2 = 0;
+	int limit_w1 = 0;
+
+	if(argc != 8){
+		cout << "Use: bin <cant nodos> <cant_aristas> <pesomin_w1> <pesomax_w1> <pesomin_w2> <pesomax_w2> <limit_w1> " << endl;
+		return 1;
+	}else{
+		cant_nodos = atoi(argv[1]);
+		cant_aristas = atoi(argv[2]);
+		pesomin_w1 = atoi(argv[3]);
+		pesomax_w1 = atoi(argv[4]);
+		pesomin_w2 = atoi(argv[5]);
+		pesomax_w2 = atoi(argv[6]);
+		limit_w1 = atoi(argv[7]);
+	}
+	int aristas_maximas = cant_nodos*(cant_nodos-1)/2;	
+
+	//validacion maximas aristas
+	if(cant_aristas > aristas_maximas){
+		cant_aristas = aristas_maximas;
+	}
+
+	//inicializo en <(0, 1), ..., (0, n-1), (1, 2), ..., (1, n-1), ..., (n - 2, n-1)>
+	vector<pair<int, int> > aristas;
+	for(int i=0;i<cant_nodos;i++){
+		for(int j=i+1;j<cant_nodos;j++){			
+			aristas.push_back(make_pair(i, j));
+		}
+	}
+
+	//ahora shufleo uniformemente con fisher yates
+	//http://en.wikipedia.org/wiki/Fisher%E2%80%93Yates_shuffle
+
+	random_device rd;
+	mt19937 gen(rd());
+
+	uniform_int_distribution<int> dis_srcdst(1, cant_nodos);
+	int nodo_src = dis_srcdst(gen);
+	int nodo_dst = dis_srcdst(gen);
+
+	//header line: nodos aristas src dst limitw1
+	cout << cant_nodos << " " << cant_aristas << " " << nodo_src << " " << nodo_dst << " " << limit_w1 << endl;
+
+	int permutations_number = aristas_maximas;//C(cant_nodos, 2)
+	uniform_int_distribution<int> dis(0, permutations_number);
+	vector<pair<int, int> >::iterator begin_iter = aristas.begin();
+	for(int i=0;i<permutations_number;i++){
+		int x = dis(gen);
+		iter_swap(begin_iter + i, begin_iter + x);
+	}
+
+	//ahora agarro del vector randomizado de aristas, las primeras cant_aristas y las imprimo con pesos aleatorios entre 
+	aristas.resize(cant_aristas);
+
+	uniform_int_distribution<int> dis_w1(pesomin_w1, pesomax_w1);
+	uniform_int_distribution<int> dis_w2(pesomin_w2, pesomax_w2);
+	for(auto arista : aristas){
+		//notemos que la salida es entre 1..n!!
+		int partida = arista.first + 1;
+		int destino = arista.second + 1;
+		int peso_w1 = dis_w1(gen);
+		int peso_w2 = dis_w2(gen);
+		cout << partida << " " << destino << " " << peso_w1 << " " << peso_w2 << endl;
+	}
+	cout << "0";
+}
