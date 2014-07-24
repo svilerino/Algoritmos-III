b0982f1734cb8af5d523c27e59b2e5d4f4b26cbf
svilerino <svilerino@gmail.com>
2014-07-23 22:43:40 -0300

    explicacion del generador fisher yates en el informe

--- tp3/codigo/heuristicas/create_tests.sh
+++ tp3/codigo/heuristicas/create_tests.sh
@@ -11,31 +11,33 @@ red='\e[0;31m'
 green='\e[0;32m'
 NC='\e[0m' # No Color
 
-cant_nodos_min=100
-cant_nodos_max=600
+cant_nodos_min=2000
+cant_nodos_max=7000
+peso_minimo_w1=0
 peso_maximo_w1=250
+peso_minimo_w2=0
 peso_maximo_w2=400
 limit_w1=200
 
-for (( n=$cant_nodos_min; n<=$cant_nodos_max; n+=10 ))
+for (( n=$cant_nodos_min; n<=$cant_nodos_max; n+=500 ))
 do 
 	#seleccionar aca abajo una combinacion de aristas... para establecer un valor fijo hay que poner max y min iguales.
 	#aristas_minimas=$((n * (n-1)/3))
 	#aristas_maximas=$((n * (n-1)/2))
 	
-	#aristas_minimas=$((n-1))
-	#aristas_maximas=$((10*n))
+	aristas_minimas=$((10*n-1))
+	aristas_maximas=$((20*n))
 	
 	#aristas_minimas=$((n * (n-1)/10))
 	#aristas_maximas=$((n * (n-1)/7))
 
-	aristas_minimas=$(echo "scale=0; $n*sqrt($n)" | bc -l )
-	aristas_maximas=$(echo "scale=0; 5*$n*sqrt($n)" | bc -l )
+	#aristas_minimas=$(echo "scale=0; $n*sqrt($n)" | bc -l )
+	#aristas_maximas=$(echo "scale=0; 5*$n*sqrt($n)" | bc -l )
 
 	echo "Generando grafos de $n nodos con cantidad de aristas aleatorias en el rango [$aristas_minimas .. $aristas_maximas]"
 	echo "Los pesos W1 de las aristas van a estar en un rango preestablecido aleatorio entre [1..$peso_maximo_w1]"
 	echo "Los pesos W2 de las aristas van a estar en un rango preestablecido aleatorio entre [1..$peso_maximo_w2]"
-	for (( a=$aristas_minimas; a<=$aristas_maximas; a+=7000 ))
+	for (( a=$aristas_minimas; a<=$aristas_maximas; a+=50000 ))
 	do
 		echo -n "Generando grafo de $n nodos y $a aristas aleatorio..."		
 		#../generar_grafos/generar_grafos "$n" "$a" "$peso_maximo_w1" "$peso_maximo_w2" "$limit_w1" > "test-cases/instancia_$n.$a.in"
